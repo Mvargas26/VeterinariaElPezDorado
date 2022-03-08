6de70444a6105e903209fe7f4a728c9bf8c2c115
Michael Vargas Soto <michaelvargas47@hotmail.com>
2022-03-08 10:35:46 -0600

    Lugar: Negocios Se crea la clase direccion, sus atributos y sus properties

---
+++ Entidades/ClienteVeterinaria.vb
@@ -0,0 +1,13 @@
+﻿Public Class ClienteVeterinaria
+#Region "Variables"
+    Dim strNombreCliente As String
+    Dim strApellidosCliente As String
+    Dim intIdentificacionCliente As Integer
+    Dim strDireccion As Direccion
+    Dim intTelefono As Integer
+    Dim strCorreoelectronico As String
+
+#End Region
+
+
+End Class
---
+++ Entidades/Direccion.vb
@@ -0,0 +1,61 @@
+﻿Public Class Direccion
+#Region "Variables"
+    Dim intCodigoProvincia As Integer
+    Dim strProvincia As String
+    Dim strCanton As String
+    Dim strDistrito As String
+    Dim strDireccionExacta As String
+
+#End Region
+
+#Region "Properties"
+
+
+    Public Property IntCodigoProvincia1 As Integer
+        Get
+            Return intCodigoProvincia
+        End Get
+        Set(value As Integer)
+            intCodigoProvincia = value
+        End Set
+    End Property
+
+    Public Property StrProvincia1 As String
+        Get
+            Return strProvincia
+        End Get
+        Set(value As String)
+            strProvincia = value
+        End Set
+    End Property
+
+    Public Property StrCanton1 As String
+        Get
+            Return strCanton
+        End Get
+        Set(value As String)
+            strCanton = value
+        End Set
+    End Property
+
+    Public Property StrDistrito1 As String
+        Get
+            Return strDistrito
+        End Get
+        Set(value As String)
+            strDistrito = value
+        End Set
+    End Property
+
+    Public Property StrDireccionExacta1 As String
+        Get
+            Return strDireccionExacta
+        End Get
+        Set(value As String)
+            strDireccionExacta = value
+        End Set
+    End Property
+
+
+#End Region
+End Class
