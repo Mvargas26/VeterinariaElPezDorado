1d25281f1ca7f9f05c7d2919ba4910d8e3d88b76
Michael Vargas Soto <michaelvargas47@hotmail.com>
2022-03-11 10:32:17 -0600

    Lugar: Interfaz Se crea la pag frmPaginaError y se prueba en el load de tipoMascota

--- VeterinariaElPezDorado/Paginas/TiposMascotas.aspx.vb
+++ VeterinariaElPezDorado/Paginas/TiposMascotas.aspx.vb
@@ -2,7 +2,13 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Try
+            '  Throw New Exception
 
+        Catch ex As Exception
+            Session("Error") = ex 'las variables de sesion permiten pasar info de una pagina a otra
+            Response.Redirect("~/Paginas/frmPaginaError", False)
+        End Try
     End Sub
 
 End Class
---
+++ VeterinariaElPezDorado/Paginas/frmPaginaError.aspx.vb
@@ -0,0 +1,20 @@
+﻿Public Class frmPaginaError
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+            Dim ex As Exception = CType(Session("Error"), Exception)
+
+            Me.lblMensaje.Text = ex.Message
+            Me.lblErrorTecnico.Text = ex.StackTrace
+        Catch ex As Exception
+            'si me genera un error al leer la variable de sesion cae aqui
+            Me.lblMensaje.Text = ex.Message
+            Me.lblErrorTecnico.Text = ex.StackTrace
+        End Try
+
+
+    End Sub
+
+End Class
